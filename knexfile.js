4b8c5e0ce807a1ee783b69a83ee72833bca51c72
Evan Smith <smith.e@salesforce.com>
2023-04-07 14:30:03 -0500

    creating a new version

--- knexfile.js
+++ knexfile.js
@@ -32,8 +32,7 @@ module.exports = {
   gcloud: {
     client: 'pg',
     connection: {
-      host: process.env.DATABASE_HOST,
-      // port: process.env.DATABASE_PORT,
+      host: process.env.INSTANCE_HOST,
       database: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD, 
